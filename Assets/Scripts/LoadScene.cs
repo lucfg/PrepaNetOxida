768abd1fd30c6a546207529ae43524ea578ecd81
Luis Carlos Flores <luiscfgmay294@gmail.com>
2017-04-21 10:28:08 -0500

    no message

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	public string destino;
+
+	public void CargaEscena() {
+		SceneManager.LoadScene(destino);
+	}
+}
